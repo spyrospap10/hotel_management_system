bc1dc53b72e8ddef408d93754c3803cb03bb0fa9
spyrospap10 <spyrospapadopoulos186@gmail.com>
2026-02-21 23:57:33 +0200

    Update CustomerController.java

--- HotelAPI/src/main/java/com/example/demo/CustomerController.java
+++ HotelAPI/src/main/java/com/example/demo/CustomerController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/api/customers")
@@ -27,4 +30,28 @@ public class CustomerController {
     public Customer createCustomer(@RequestBody Customer customer) {
         return customerRepository.save(customer);
     }
+
+    @DeleteMapping("/{id}")
+    public void deleteCustomer(@PathVariable Integer id) {
+        customerRepository.deleteById(id);
+    }
+
+    // 2. Ενημέρωση (Επεξεργασία) Πελάτη
+    @PutMapping("/{id}")
+    public Customer updateCustomer(@PathVariable Integer id, @RequestBody Customer updatedCustomer) {
+        return customerRepository.findById(id)
+                .map(customer -> {
+                    customer.setId(updatedCustomer.getId());
+                    customer.setLastName(updatedCustomer.getLastName());
+                    customer.setFirstName(updatedCustomer.getFirstName());
+                    customer.setNationality(updatedCustomer.getNationality());
+                    customer.setPassport(updatedCustomer.getPassport());
+                    customer.setGender(updatedCustomer.getGender());
+                    customer.setBirth(updatedCustomer.getBirth());
+                    customer.setPhoneNumber(updatedCustomer.getPhoneNumber());
+                    customer.setEmail(updatedCustomer.getEmail());
+                    return customerRepository.save(customer);
+                }).orElseThrow(() -> new RuntimeException("Customer not found"));
+    }
+
 }
